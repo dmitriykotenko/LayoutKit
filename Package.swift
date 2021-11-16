4554d332007055b2307bb23d9246878bc139bf7b
Dmitry Kotenko <d.kotenko@yandex.ru>
2021-11-16 18:43:00 +0300

    Package.swift updated to Swift 5.3

--- Package.swift
+++ Package.swift
@@ -1,3 +1,5 @@
+// swift-tools-version:5.3
+
 // Copyright 2017 LinkedIn Corp.
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -6,10 +8,24 @@
 // software distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
-// swift-tools-version:3.1
 
 import PackageDescription
 
 let package = Package(
-    name: "LayoutKit"
+  name: "LayoutKit",
+  platforms: [.iOS(.v11)],
+  products: [
+    .library(
+      name: "LayoutKit",
+      targets: [
+        "LayoutKit",
+      ]
+    )
+  ],
+  targets: [
+    .target(
+      name: "LayoutKit",
+      path: "Sources"
+    )
+  ]
 )
